d992728782e078b0abf93baeb9378a6b6b311624
snfazal <snfazal@gmail.com>
2017-03-15 11:47:18 -0400

    removed name, users controller tested and working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,41 +11,41 @@ class UsersController < ApplicationController
     end
   end
 
-#   def show
-#     render json: { status: 200, user: current_user }
-#   end
-#
-#   def login
-#     user = User.find_by(email: params[:user][:email])
-#
-#     if user && user.authenticate(params[:user][:password])
-#       token = token(user.id, user.email)
-#
-#       render json: { status: 201, user: user, token: token }
-#     else
-#       render json: { status: 401, message: "authorized" }
-#     end
-#   end
-#
-#   private
-#
-#   def token(id, email)
-#     JWT.encode(payload(id, email), 'bestsecretever', 'HS256')
-#   end
-#
-#   def payload(id, email)
-#     {
-#       exp: (Time.now + 1.day).to_i,
-#       iat: Time.now.to_i,
-#       iss: 'wdir-matey',
-#       user: {
-#         id: id,
-#         email: email
-#     }
-#   end
-#
-#   def user_params
-#     params.required(:user).permit(:name, :username, :email, :password)
-#   end
-#
+  def show
+    render json: { status: 200, user: current_user }
+  end
+
+  def login
+    user = User.find_by(email: params[:user][:email])
+
+    if user && user.authenticate(params[:user][:password])
+      token = token(user.id, user.email)
+
+      render json: { status: 201, user: user, token: token }
+    else
+      render json: { status: 401, message: "authorized" }
+    end
+  end
+
+  private
+
+  def token(id, email)
+    JWT.encode(payload(id, email), 'bestsecretever', 'HS256')
+  end
+
+  def payload(id, email)
+    {
+      exp: (Time.now + 1.day).to_i,
+      iat: Time.now.to_i,
+      iss: 'wdir-matey',
+      user: {
+        id: id,
+        email: email
+    }
+  end
+
+  def user_params
+    params.required(:user).permit(:username, :email, :password)
+  end
+
 end
