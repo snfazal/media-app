d122810df0c543afd05877ad5f084bc839548ef0
snfazal <snfazal@gmail.com>
2017-03-15 10:37:19 -0400

    cleaned up auth in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,30 @@
 class ApplicationController < ActionController::API
-  # is the user logged in
   def authenticate
-    puts 'authenticate'
-    render json: {status: 401, message: "authorized"} unless decode_token(bearer_token)
-  end
+     render json: {status: 401, message: "unauthorized"} unless decode_token(bearer_token)
+   end
 
-  def test
-    puts "MADE"
-  end
+   def authorize
+     render json: {status: 401, message: "unauthorized"} unless current_user.id == params[:id].to_i
+   end
 
-  # does the user own the resource they're asking for
-  def authorize
-    render json: {status: 401, message: "unauthorized"} unless current_user.id == params[:id].to_i
-  end
+   def bearer_token
+     pattern = /^Bearer /
+     header  = request.env["HTTP_AUTHORIZATION"] # <= env
+     header.gsub(pattern, '') if header && header.match(pattern)
+   end
 
-  def authorize_media
-    puts current_user.id
-    render json: {status: 401, message: "unauthorized"} unless current_user.id == params[:user_id].to_i
-  end
+   def current_user
+     return if !bearer_token
 
-  def bearer_token
-    pattern = /^Bearer /
-    header  = request.env["HTTP_AUTHORIZATION"] # <= env
-    header.gsub(pattern, '') if header && header.match(pattern)
-  end
+     decoded_jwt = decode_token(bearer_token)
 
-  def current_user
-    return if !bearer_token
+     @current_user ||= User.find(decoded_jwt["user"]["id"])
+   end
 
-    decoded_jwt = decode_token(bearer_token)
+   def decode_token(token)
+     @token ||= JWT.decode(token, nil, false)[0]
 
-    @current_user ||= User.find(decoded_jwt["user"]["id"])
-  end
-
-  def decode_token(token)
-    @token ||= JWT.decode(token, nil, false)[0] # error if the token is invalid or expired
-  rescue
-    render json: {status: 401, message: 'invalid or expired token'}
-  end
+   rescue
+     render json: {status: 401, message: 'invalid or expired token'}
+   end
 end
