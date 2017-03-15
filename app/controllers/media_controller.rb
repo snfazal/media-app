3e894e3a115305c5882ba47cf432c2c7df1ad528
snfazal <snfazal@gmail.com>
2017-03-15 15:13:12 -0400

    FULL CRUD works, params were missing

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -14,8 +14,30 @@ class MediaController < ApplicationController
 
   def show
    render json: { status: 200, media: Media.find(params[:id]) }
- end
+  end
 
- def index
+  def index
    render json: { status: 200, media: Media.all }
- end
+  end
+
+  def update
+    media = Media.find(params[:id])
+
+    if media.update(media_params)
+      render json: { status: 422, user: media.errors }
+    else
+      render json: { status: 422, user: media.errors }
+    end
+  end
+
+  def destroy
+    Media.destroy(params[:id])
+
+    render json: { status: 204, message: 'resource deleted successfully', media: current_user.media }
+  end
+
+
+  def media_params
+    params.required(:media).permit( :name, :linkable_type, :linkable_id)
+  end
+end
