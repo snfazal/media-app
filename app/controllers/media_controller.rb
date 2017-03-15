45b1a0a6315c7d66a9acf94e700f496fe165e7b8
snfazal <snfazal@gmail.com>
2017-03-15 13:28:32 -0400

    able to create and show media from controller

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,9 +1,21 @@
 class MediaController < ApplicationController
+  before_action :authorize_media, only: [:update, :delete]
 
-  # def index
-  #   render json: Media.all
-  # end
+  def create
+    puts media_params
+    media = current_user.media.new(media_params)
+    puts media
+    if media.save
+      render json: { status: 201, media: media}
+    else
+      render json: { status: 422, user: media.errors}
+    end
+  end
 
-  # def show
-  #   render json: Media.find(params[:id])
-  # end
+  def show
+   render json: { status: 200, media: Media.find(params[:id]) }
+ end
+
+ def index
+   render json: { status: 200, media: Media.all }
+ end
