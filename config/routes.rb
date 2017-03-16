35b6866d7f390cf948fa49cf5e5017164068bfc6
snfazal <snfazal@gmail.com>
2017-03-16 16:24:06 -0400

    update resources in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:create, :show, :destroy] do
+    collection do
+      post '/login', to: 'users#login'
+    end
+  end
   resources :media
 end
