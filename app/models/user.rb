1ab8bc9b371787e417d650eb1db298008d929eb5
snfazal <snfazal@gmail.com>
2017-03-17 10:14:58 -0400

    reconfiguring backend models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
   has_many :links, as: :linkable
+
+  has_many :notes, dependent: :destroy
+  has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,3 +7,16 @@ Rails.application.routes.draw do
   end
   resources :media
 end
+
+# Below are Rails Routes for Reference
+# Prefix Verb   URI Pattern            Controller#Action
+# login_users POST   /users/login(.:format) users#login
+#  users POST   /users(.:format)       users#create
+#   user GET    /users/:id(.:format)   users#show
+#        DELETE /users/:id(.:format)   users#destroy
+#  media GET    /media(.:format)       media#index
+#        POST   /media(.:format)       media#create
+# medium GET    /media/:id(.:format)   media#show
+#        PATCH  /media/:id(.:format)   media#update
+#        PUT    /media/:id(.:format)   media#update
+#        DELETE /media/:id(.:format)   media#destroy
